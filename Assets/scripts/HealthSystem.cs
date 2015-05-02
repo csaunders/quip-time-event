28cd6276c98337f04d68c9bb6391e93eb078eaa1
Chris Saunders <chris.saunders@shopify.com>
2015-05-02 18:21:50 +0000

    adding stuff

---
+++ Assets/scripts/HealthSystem.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HealthSystem : MonoBehaviour {
+
+	public static int MAX_HEALTH = 2000;
+
+	public int _currentHealth;
+	public Text healthDisplay;
+
+	// Use this for initialization
+	public void Start () {
+		_currentHealth = MAX_HEALTH;	
+	}
+
+	public void Update() {
+		healthDisplay = "HP: " + _currentHealth;
+	}
+
+	public void InflictDamage(float amount)
+	{
+		_currentHealth -= amount;
+	}
+
+	public bool IsDead(){
+		return _currentHealth <= 0.0f;
+	}
+}
