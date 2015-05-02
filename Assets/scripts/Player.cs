1fd68c658872fecd408c800d61a0c356f4e64d45
Chris Saunders <chris.saunders@shopify.com>
2015-05-02 21:11:53 +0000

    restarting git history

---
+++ Assets/scripts/Player.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : HealthSystem {
+	
+	private float _damage;
+	private Computer _computer;
+	private QuipSystem system;
+
+	void Start () {
+		base.Start ();
+		_damage = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		base.Update ();
+		updateReferences ();
+		if (system == null) {
+			system = QuipSystem.GodObject;
+		}
+
+		if (system.Tracker.DonePhrase) {
+			_computer.InflictDamage(_damage);
+			_damage = 0.0f;
+		}
+
+		if (Input.GetButtonDown(system.Tracker.NextButton))
+		{
+			_damage += system.Tracker.Score();
+		}
+	}
+
+	private void updateReferences()
+	{
+		if (_computer != null) {
+			return;
+		}
+		Debug.Log ("Computer is still null");
+		_computer = (Computer) GameObject.Find ("Computer").GetComponent (typeof(Computer));
+	}
+}
---
+++ Assets/scripts/QuipSystem.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class QuipSystem : MonoBehaviour {
+
+	public static QuipSystem GodObject;
+	
+	public Text quipPlayer;
+	public Text quipGuide;
+	public Text successText;
+	public Text debugLog;
+	public float characterRenderSpeed = 0.30f;
+	public float accuracyBuffer = 0.60f;
+	
+	private string phrase = "some peo%ple just wa%nt to wat%ch th%e world bu%rn.";
+	private QuickTimeTracker tracker;
+	public QuickTimeTracker Tracker { get { return tracker; } }
+
+	// Use this for initialization
+	void Start () {
+		GodObject = this;
+		Reset ();
+	}
+
+	public void Reset() {
+		tracker = new QuickTimeTracker (phrase, characterRenderSpeed, accuracyBuffer);
+
+		quipPlayer.text = "";
+		quipGuide.text = tracker.FullMessage ();
+		successText.text = "--- Nothing ---";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		handleInput ();
+		tracker.Update (Time.deltaTime);
+		quipPlayer.text = tracker.PartialMessage();
+		debugData ();
+	}
+
+	private void handleInput()
+	{
+		if (Input.GetKeyDown (KeyCode.R)) {
+			Reset ();
+		}
+		if (Input.GetKeyDown (KeyCode.F)) {
+		}
+	}
+
+	private void debugData()
+	{
+		debugLog.text = "";
+		debugLog.text += string.Format ("Timer: {0}\n", tracker.Timer);
+		debugLog.text += string.Format ("Closest Time: {0}\n", tracker.ClosestTime);
+		debugLog.text += string.Format ("Score: {0}\n", tracker.Score());
+		debugLog.text += string.Format ("All Times: {0}\n", tracker.AllTimes);
+
+		foreach (string qte in tracker.QuickTimeStrings()) {
+			drawCursor(qte, quipPlayer);
+		}
+	}
+
+	//http://answers.unity3d.com/questions/37987/gui-text-object-width.html
+	private void drawCursor(string msg, Text txt)
+	{
+		GUIStyle style = new GUIStyle ();
+		style.font = txt.font;
+		style.fontSize = txt.fontSize;
+		Vector3 position = txt.transform.position;
+		Rect rect = txt.rectTransform.rect;
+		Vector2 size = style.CalcSize(new GUIContent(msg));
+
+		Vector2 sizeOfPercentSymbol = style.CalcSize (new GUIContent ("%"));
+		Vector3 start = new Vector3 (position.x + rect.x + size.x - (sizeOfPercentSymbol.x / 2), position.y - 10, 100);
+		Vector3 end = new Vector3(position.x + rect.x + size.x - (sizeOfPercentSymbol.x / 2), position.y + 10, 100);
+		Debug.DrawLine(start, end, Color.white);
+	}
+}
